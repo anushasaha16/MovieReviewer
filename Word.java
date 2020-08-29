b066b12cf8b93d37cc51eced206039111d39d305
Anusha Saha <44592491+anushasaha16@users.noreply.github.com>
2020-08-29 09:53:49 -0500

    Update Word.java

--- Word.java
+++ Word.java
@@ -1,8 +1,13 @@
+/**
+ * Word class provides methods to calculate a word's sentiment rating
+ * @author anush
+ *
+ */
 public class Word
 {
-	private String str;
-	private int totalWeight;
-	private int totalCount;
+	private String str;	//string representation of the word
+	private int totalWeight;//sum of all the ratings of previous reviews Word has been used in
+	private int totalCount;	//keeps track of how many times the word has been used before
 
 	public Word(String s, int w, int c)
 	{
@@ -10,32 +15,55 @@ public class Word
 		totalWeight = w;
 		totalCount = c;
 	}
-
+	
+	/**
+	 * 
+	 * @return	string representation of the word
+	 */
 	public String getText()
 	{
 		return str;
 	}
-
+	
+	/**
+	 * 
+	 * @return integer totalWeight
+	 */
 	public int getTotalWeight()
 	{
 		return totalWeight;
 	}
-
+	
+	/**
+	 * Every time Word is used, totalWeight is increased by the rating of the review Word was used in
+	 * @param r	rating of review Word was used in
+	 */
 	public void addToTotalWeight(int r)
 	{
 		totalWeight = totalWeight + r;
 	}
-
+	
+	/**
+	 * 
+	 * @return integer totalCount which keeps track of how many times the word has been used before
+	 */
 	public int getTotalCount()
 	{
 		return totalCount;
 	}
-
+	
+	/**
+	 * increments the totalCount
+	 */
 	public void addToTotalCount()
 	{
 		totalCount++;
 	}
-
+	
+	/**
+	 * Calls the methods above in order to calculate the sentiment rating of an instance of Word
+	 * @return sentiment rating of Word
+	 */
 	public int getSentimentRating()
 	{
 		double r = this.getTotalWeight()/this.getTotalCount();
